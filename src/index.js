0d2f64d81ff85f3fd608d494cd3976b82ac084ab
Valeriy95 <vmariukhnyk@gmail.com>
2023-01-21 15:33:50 +0400

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+      let open = [];
+      let close = new Object();
+      let stack = [];
+      
+      bracketsConfig.flat().forEach((v, i, arr) => ( i % 2 == 0) ? open.push(v) : close[v] = arr[i - 1]);
+      
+      for (let i = 0; i < str.length; i++) {
+         let currentSymbol = str[i];
+         if(open.includes(currentSymbol)) {
+            if(stack.includes('|') && currentSymbol == '|') {
+               stack.pop();
+            } else if(stack.includes('7') && currentSymbol == '7') {
+               stack.pop();
+            } else if(stack.includes('8') && currentSymbol == '8') {
+               stack.pop();
+            } else {
+            stack.push(currentSymbol);
+            }
+         } else {
+            if (stack.length === 0) {
+               return false;
+            }
+   
+            let topElement = stack[stack.length - 1];
+   
+            if (close[currentSymbol] === topElement) {
+               stack.pop();
+            } else {
+               return false;
+            }
+         }
+      }
+      return stack.length === 0;  
+   };
+   
